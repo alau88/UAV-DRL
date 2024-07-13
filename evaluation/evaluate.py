f267cde135c885c50ad5b08c74725182f81493ac
ut-ruonan <ruby.niu@mail.utoronto.ca>
2024-07-13 19:44:34 -0400

    Cleared warning with clone().detach()

--- evaluation/evaluate.py
+++ evaluation/evaluate.py
@@ -22,8 +22,7 @@ def evaluate_policy(env, policy_net, num_episodes=1, device='cpu'):
                 action = policy_net(state).cpu().numpy().flatten()
 
             next_state, reward, done, _ = env.step(action.reshape(env.num_uavs, 2))
-            next_state = torch.tensor(next_state.flatten(), dtype=torch.float32).unsqueeze(0).to(device)
-
+            next_state = next_state.flatten().clone().detach().unsqueeze(0).to(device)
             next_state_np = next_state.cpu().numpy().flatten()
             user_positions = next_state_np[:env.num_users * 2].reshape(env.num_users, 2)
             uav_positions = next_state_np[env.num_users * 2:].reshape(env.num_uavs, 2)
