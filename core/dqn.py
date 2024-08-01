e57da4b8115dbf44765feecb8dd3becec91bfb5d
Anson Lau <anson.lau@live.com>
2024-08-01 19:10:23 -0400

    update for dueling dqn given most recent uavenv

--- core/dqn.py
+++ core/dqn.py
@@ -31,15 +31,18 @@ class DQN(nn.Module):
 
 
 class DuelingDQN(nn.Module):
-    def __init__(self, state_dim, action_dim):
+    def __init__(self, state_dim, num_uavs, action_dim):
         super(DuelingDQN, self).__init__()
+        self.num_uavs = num_uavs
+        self.action_dim = action_dim
+
         self.fc1 = nn.Linear(state_dim, 256)
         self.bn1 = nn.BatchNorm1d(256)
         self.fc_value = nn.Linear(256, 256)
         self.fc_advantage = nn.Linear(256, 256)
         
         self.value = nn.Linear(256, 1)
-        self.advantage = nn.Linear(256, action_dim)
+        self.advantage = nn.Linear(256, num_uavs * action_dim)
         self.dropout = nn.Dropout(p=0.2)
 
     def forward(self, x):
@@ -56,4 +59,4 @@ class DuelingDQN(nn.Module):
         advantage = self.advantage(advantage)
         
         q_vals = value + (advantage - advantage.mean(dim=1, keepdim=True))
-        return q_vals
+        return q_vals.view(-1, self.num_uavs, self.action_dim)
--- evaluation/hyperparam_tuning.py
+++ evaluation/hyperparam_tuning.py
@@ -17,15 +17,15 @@ param_names = list(param_grid.keys())
 
 
 def evaluate_config(config, network):
-    env = UAVEnv(num_users=20, num_uavs=3, area_size=(100, 100))
+    env = UAVEnv(num_users=10, num_uavs=3, area_size=(100, 100))
     state_size = env.observation_space.shape[0] * env.observation_space.shape[1]
     action_size = env.action_space.n
     if network == "DQN":
         policy_net = DQN(state_size, env.num_uavs, action_size).to(device)
         target_net = DQN(state_size, env.num_uavs, action_size).to(device)
     elif network == "DuelingDQN":
-        policy_net = DuelingDQN(state_size, action_size).to(device)
-        target_net = DuelingDQN(state_size, action_size).to(device)
+        policy_net = DuelingDQN(state_size, env.num_uavs, action_size).to(device)
+        target_net = DuelingDQN(state_size, env.num_uavs, action_size).to(device)
     else:
         raise Exception("Please choose valid DQN class from ./core/dqn.py")
     target_net.load_state_dict(policy_net.state_dict())
